a4a3ab32bf5a00bfcd2be771900f2f5bc210f1fb
mpaulweeks <mpaulweeks@gmail.com>
2022-03-24 11:44:47 -0400

    add shared project

---
+++ shared/src/types.ts
@@ -0,0 +1,32 @@
+export type EventKey = string;
+export interface EventTime {
+  eid: EventKey;
+  start: Date;
+  durationHours: number;
+}
+export enum Attendence {
+  No = 'n',
+  Yes = 'y',
+  Maybe = 'm',
+  Undefined = '?',
+}
+export const AttendenceOrder = [
+  Attendence.No,
+  Attendence.Yes,
+  Attendence.Maybe,
+];
+export interface EventAttendence {
+  event: EventKey;
+  status: Attendence;
+}
+export interface User {
+  uid: string;
+  name: string;
+  events: EventAttendence[];
+  created: Date;
+  updated: Date;
+}
+export interface Schedule {
+  events: EventTime[];
+  users: User[];
+}
