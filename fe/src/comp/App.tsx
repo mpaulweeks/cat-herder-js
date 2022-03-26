463b201adb4333a916e0acf1fdbbe2bd9e62cd2a
mpaulweeks <mpaulweeks@gmail.com>
2022-03-26 19:17:13 -0400

    progress on fe schedule

--- fe/src/comp/App.tsx
+++ fe/src/comp/App.tsx
@@ -1,31 +1,10 @@
-import { getThisWeek, AttendenceOrder, EventTime, Schedule } from "@mpaulweeks/cat-shared"
 import { ScheduleTable } from "./ScheduleTable";
 import './App.css';
+import { createSchedule } from "../lib/schedule";
 
 export function App(props: {}) {
   // todo get from api
-  const data: Schedule = {
-    events,
-    users: [{
-      uid: '1',
-      name: 'Bob',
-      events: events.map((et, ei) => ({
-        event: et.eid,
-        status: AttendenceOrder[ei % AttendenceOrder.length],
-      })),
-      created: new Date(),
-      updated: new Date(),
-    },{
-      uid: '2',
-      name: 'Alice',
-      events: events.map((et, ei) => ({
-        event: et.eid,
-        status: AttendenceOrder[(ei + 1) % AttendenceOrder.length],
-      })),
-      created: new Date(),
-      updated: new Date(),
-    }],
-  };
+  const data = createSchedule();
   return (
     <ScheduleTable
       data={data}
--- fe/src/lib/schedule.ts
+++ fe/src/lib/schedule.ts
@@ -1,25 +1,36 @@
-import { EventDate } from "@mpaulweeks/cat-shared";
+import { EventDate, range, Schedule } from "@mpaulweeks/cat-shared";
 
 export function createSchedule(args?: {
   group: string;
   dateStr: string;
-}) {
+}): Schedule {
   const group = args?.group ?? 'edh';
-  const dateStr = args?.dateStr ?? getPreviousMonday(new Date());
+  const ed = args?.dateStr ? EventDate.fromStr(args.dateStr) : EventDate.now();
+  return scheduleByGroup(group, ed);
 }
 
-export function eventsByGroup(group: string, ed: EventDate) {
+export function scheduleByGroup(group: string, ed: EventDate): Schedule {
   if (group === 'edh') {
-
-    return getThisWeek(date, {
-      hours: 18,
-      minutes: 0,
-      seconds: 0,
-    }).map(d => ({
-      eid: d.getTime().toString(),
-      start: d,
+    const events = range(7).map(i => {
+      const newDate = new Date(ed.date);
+      newDate.setDate(newDate.getDate() + i);
+      const newEd = EventDate.fromDate(newDate);
+      return newEd.getDateAtHour({
+        hours: 18,
+        minutes: 0,
+      });
+    }).map(date => ({
+      eid: date.getTime().toString(),
+      start: date,
       durationHours: 4,
     }));
+    return {
+      sid: ed.dateStr,
+      name: 'EDH',
+      description: '',
+      events,
+      users: [],
+    }
   }
   // else
   throw new Error('unsupported group');
