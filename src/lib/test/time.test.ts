93f498c62f5f49a7e726f0b863c3d5e8eb698520
mpaulweeks <mpaulweeks@gmail.com>
2023-07-05 11:36:20 -0400

    switch to vitest, add gh action

--- src/lib/test/time.test.ts
+++ src/lib/test/time.test.ts
@@ -1,14 +1,13 @@
+import { expect, test } from 'vitest';
 import { EventDate } from "../time";
 
-describe('EventDate', () => {
-  test('dateIso', () => {
+test('dateIso', () => {
     const ed = EventDate.now();
     expect(ed.dateIso).toBe(ed.date.toISOString());
     expect(ed.equals(EventDate.fromIso(ed.dateIso))).toBe(true);
   });
 
-  test('dateStr', () => {
-    const ed = EventDate.fromIso('2022-03-29T17:45:51.007Z');
-    expect(ed.dateStr).toBe('20220329');
-  });
+test('dateStr', () => {
+  const ed = EventDate.fromIso('2022-03-29T17:45:51.007Z');
+  expect(ed.dateStr).toBe('20220329');
 });
